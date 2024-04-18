6dfacc1e2ec62bbb8da83fbcb5dffa308daf3267
huamaoorz <151900045+huamaoorz@users.noreply.github.com>
2024-04-18 16:28:03 +0800

    text

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-PF_number = "PF02298"
+PF_number = "PF00170"
 
 evaluation_threshold = "1e-5"
 
-species = "Arabidopsis_thaliana"
+species = "Wm82"
 
 # The repository only contains the genome protein sequences of three species: Arabidopsis_thaliana , Oryza_sativa , Lolium_perenne , and Zea_mays
